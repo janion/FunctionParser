e49f19c1155734eb80af91c950cb6930d24e35ff
Janion <Janion@192.168.2.3>
2017-12-26 21:54:47 +0000

    Updated exception

--- FunctionParser/src/fn/exception/NoVariableException.java
+++ FunctionParser/src/fn/exception/NoVariableException.java
@@ -4,8 +4,15 @@ public class NoVariableException extends RuntimeException {
 
 	private static final long serialVersionUID = -135178990867016454L;
 	
+	private String variableName;
+	
 	public NoVariableException(String variableName) {
 		super(String.format("No value for variable %s", variableName));
+		this.variableName = variableName;
+	}
+	
+	public String getVariableName() {
+		return variableName;
 	}
 
 }
